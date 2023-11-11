8bf92d4a2c13645dc8a8a852d936a9520b6d527b
Jiří Klepl <klepl@d3s.mff.cuni.cz>
2023-11-11 21:56:17 +0100

    updatew the build script

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # Create the build directory
-cmake -E make_directory build || exit 1
-cd build || exit 1
+cmake -E make_directory build
+cd build
+
+DATASET_SIZE=${DATASET_SIZE:-EXTRALARGE}
+DATA_TYPE=${DATA_TYPE:-FLOAT}
 
 # Configure the build
 cmake -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_FLAGS="-DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -DLARGE_DATASET -DDATA_TYPE_IS_DOUBLE -D_POSIX_C_SOURCE=200809L " \
-    .. \
-    || exit 1
+    -DCMAKE_C_FLAGS="-DPOLYBENCH_TIME -DPOLYBENCH_DUMP_ARRAYS -D${DATASET_SIZE}_DATASET -DDATA_TYPE_IS_$DATA_TYPE -D_POSIX_C_SOURCE=200809L " \
+    ..
 
 # Build the project
 NPROC=$(nproc)
-cmake --build . --config Release -j"$NPROC" || exit 1
+cmake --build . --config Release -j"$NPROC"
